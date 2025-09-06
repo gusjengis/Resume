6d7b7ebdefe67d0ced3a97da2d6c96a00502f783
gusjengis <anthony.j.green@outlook.com>
2025-09-06 14:07:19 -0700

    included font files in repo because pc was picking outline font for bold, removed that one from the set

--- compiler.sh
+++ compiler.sh
@@ -3,16 +3,16 @@ set -e
 
 mkdir -p out
 
-typst watch src/resume.typ out/resume.pdf  --input rust=false &
+typst watch src/resume.typ out/resume.pdf --font-path fonts --ignore-system-fonts --input rust=false &
 PID1=$!
 
-typst watch src/resume.typ out/resume-rust.pdf --input rust=true > /dev/null 2>&1 &
+typst watch src/resume.typ out/resume-rust.pdf --font-path fonts --ignore-system-fonts --input rust=true > /dev/null 2>&1 &
 PID2=$!
 
-typst watch src/resume.typ out/resume.png --input rust=false > /dev/null 2>&1 &
+typst watch src/resume.typ out/resume.png --font-path fonts --ignore-system-fonts --input rust=false > /dev/null 2>&1 &
 PID3=$!
 
-typst watch src/resume.typ out/resume-rust.png --input rust=true > /dev/null 2>&1 &
+typst watch src/resume.typ out/resume-rust.png  --font-path fonts --ignore-system-fonts --input rust=true > /dev/null 2>&1 &
 PID4=$!
 
 trap "kill $PID1 $PID2 $PID3 $PID4" SIGINT SIGTERM
